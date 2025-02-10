ee0f073d3a81bfb89ae357219d279a4114c45ba4
rashmiisurya <grashmisurya@gmail.com>
2025-02-10 15:47:20 +0530

    inventory_price_update committed

---
+++ leetcode/inventory_price_update.js
@@ -0,0 +1,21 @@
+const products = [
+    { id: 1, name: "Widget", price: 50, quantity: 120 },
+    { id: 2, name: "Gadget", price: 30, quantity: 80 },
+    { id: 3, name: "Doohickey", price: 100, quantity: 200 },
+    { id: 4, name: "Thingamajig", price: 75, quantity: 90 }
+];
+
+const updatedprod = products.map(product=>{
+    if(product.quantity>=100){
+        return {...product, price: product.price * 0.9 }
+    }
+    return product;
+});
+
+const discountedprod = products.filter(product=>{
+    product.quantity>100;
+});
+
+discountedprod.forEach(product =>{
+    console.log("Product : `$(product.name)`, Price : `$(product.price)`");
+})
